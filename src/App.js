b873b8e20b500dc866f84f3dd95e25310769b0d2
himanshu <Himanshurwt@gmail.com>
2024-11-14 01:16:23 +0530

    let start with frontent

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
