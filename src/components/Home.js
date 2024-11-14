9b93c53dc9fedd1b86935f717e303641ffef0ddd
himanshu <Himanshurwt@gmail.com>
2024-11-15 01:44:01 +0530

    use fetchall notes with notestate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,36 @@
-import React from 'react'    //rafc//
-
+import React, { useContext } from 'react'    //rafc//
+import noteContext from "../context/notes/noteContext"
 const Home = () => {
+  const context = useContext(noteContext);
+  const {notes ,setnotes} =context;
   return (
     <div>
-      <div className="container my-3 ">
+      <div className="container my-3">
         <h2> Add your Notes</h2>
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <form className="my-3">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" />
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
           </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
       <div className="container" my-3>
-        <h1> your Note</h1>
+        <h2> your Note</h2>
+              
+                  {notes.map((note) => {
+              return note.title;
+            })
+            }
       </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,40 @@
   //Create NoteState that can aceesable all files //;    
+import { useState } from "react";
 import NoteContext from "./noteContext"; // Ensure this path is correct
 
 const NoteState = (props) => {
-
+         const notesinital = [
+                {
+                  "_id": "67365102ae0046716c001925",
+                  "user": "67311f43195528664a3cdb1a",
+                  "title": "this is my title",
+                  "description": "api has been finished",
+                  "tag": "complete",
+                  "date": "2024-11-14T19:35:30.466Z",
+                  "__v": 0
+                },
+                {
+                  "_id": "67365109ae0046716c001928",
+                  "user": "67311f43195528664a3cdb1a",
+                  "title": "3",
+                  "description": "api has been finished",
+                  "tag": "complete",
+                  "date": "2024-11-14T19:35:37.942Z",
+                  "__v": 0
+                },
+                {
+                  "_id": "6736510cae0046716c00192a",
+                  "user": "67311f43195528664a3cdb1a",
+                  "title": "3",
+                  "description": "api has been finished",
+                  "tag": "complete",
+                  "date": "2024-11-14T19:35:40.077Z",
+                  "__v": 0
+                }
+              ]
+         const[notes, setNotes] = useState(notesinital)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes , setNotes}}>  {/*access Notes and setnotes in everywhere where i want */}
             {props.children}
         </NoteContext.Provider>
     );
