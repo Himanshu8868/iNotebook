647843a133e62fdedf9958b7b348485dfb4e2057
himanshu <Himanshurwt@gmail.com>
2024-11-14 23:33:59 +0530

    connect frontent with ripo and use location

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,9 @@
 import React, { useContext, useEffect } from 'react';
-import NoteContext from '../context/notes/noteContext'; // Import NoteContext for file access
 
 const About = () => {
-  const a = useContext(NoteContext); // Access the context from NoteContext
-   useEffect(() => {
-      a.update();
-      // eslint-disable-next-line
-   }, [])
   return (
     <div>
-      This is About {a.state.name} and class {a.state.class}
+      This is About page
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,24 +1,10 @@
-  //Create NoteState that can aceesable all files //
-import React, { useState } from "react";    
+  //Create NoteState that can aceesable all files //;    
 import NoteContext from "./noteContext"; // Ensure this path is correct
 
 const NoteState = (props) => {
-    const s1 = {
-        name: "Himanshu",
-        class: "1A"
-    };
-    const [state , setState] = useState(s1)
-    const update = () => {
-      setTimeout(() => {
-         setState({
-            name: "Himanshu",
-            class:"21"
-         })
-      }, 2000);
-    }
 
     return (
-        <NoteContext.Provider value={{state , update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     );
